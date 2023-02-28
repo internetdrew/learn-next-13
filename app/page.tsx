c9dbc6399a3406fccc4d47a5d9d200c56371443f
internetdrew <andrew@internetdrew.com>
2023-02-28 00:28:44 -0500

    add streaming via suspense to keep data in motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import TodosList from './(users)/todos/TodosList';
 
 const Home = () => {
-  return <div className='text-red-500'>Home</div>;
+  return (
+    <div className='flex flex-col space-y-10'>
+      <Suspense fallback={<p>Loading the todos...</p>}>
+        <div className='flex space-x-2'>
+          {/* @ts-ignore */}
+          <TodosList />
+        </div>
+      </Suspense>
+
+      <Suspense fallback={<p>Loading the shopping cart...</p>}>
+        <div className='flex space-x-2'>
+          {/* @ts-ignore */}
+          <TodosList />
+        </div>
+      </Suspense>
+    </div>
+  );
 };
 
 export default Home;
