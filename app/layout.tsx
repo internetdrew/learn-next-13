6b872a4acfa4ba6f939c72e2c2ca5e4215eee4ef
internetdrew <andrew@internetdrew.com>
2023-02-27 23:44:26 -0500

    add new relative head component

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <head>
+        <title>Testing Next 13</title>
+      </head>
       <body>
         <Header />
         {children}
--- app/search/[searchTerm]/page.tsx
+++ app/search/[searchTerm]/page.tsx
@@ -20,6 +20,8 @@ const search = async (searchTerm: string) => {
   const res = await fetch(
     `https://serpapi.com/search.json?q=${searchTerm}&api_key=${process.env.API_KEY}`
   );
+  throw new Error('Whoops. Something broke.');
+
   const data: SearchResult = await res.json();
   return data;
 };
