af342265315c5bb259c496e813fd019c6688a054
internetdrew <andrew@internetdrew.com>
2023-02-28 00:01:12 -0500

    reorganize directories for organization

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,9 +4,15 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <div className='p-5 bg-blue-500'>
-      <Link href='/' className='px-2 py-1 bg-white text-blue-500 rounded-lg'>
-        Home
-      </Link>
+      {['home', 'todos', 'search'].map((page, i) => (
+        <Link
+          key={i}
+          href={`/${page === 'home' ? '' : page}`}
+          className='px-2 py-1 bg-white text-blue-500 rounded-lg mr-2'
+        >
+          {page}
+        </Link>
+      ))}
     </div>
   );
 };
